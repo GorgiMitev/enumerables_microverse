4676f421cfc35edc8643a766b9137282b2e42ffe
Gorgi <gorgimitev@gmail.com>
2021-04-02 10:58:04 +0200

    updating rubocop file

--- Gemfile
+++ Gemfile
@@ -1,8 +1,7 @@
-# frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+git_source(:github) { |enumerables_microverse| "https://github.com/Solenka1#{enumerables_microverse}" }
 
 # gem "rails"
 gem 'rspec'
--- enumerables.rb
+++ enumerables.rb
@@ -1,7 +1,7 @@
 module Enumerable
   def my_each
     i = 0
-    while i < self.length
+    while i < length
       yield self[i]
       i += 1
     end
@@ -11,116 +11,103 @@ module Enumerable
 
   def my_each_with_index
     i = 0
-    while i < self.length
+    while i < length
       yield(self[i], i)
       i += 1
     end
   end
 
-# [1, 2, 3, 4, 5].my_each_with_index { |i, index| puts i.to_s + " index:" + index.to_s }
+  # [1, 2, 3, 4, 5].my_each_with_index { |i, index| puts i.to_s + " index:" + index.to_s }
 
   def my_select
     i = 0
-    while i < self.length
+    while i < length
       yield(self[i])
       i += 1
     end
   end
 
-#[1, 2, 3, 4, 5].my_select { |i| puts i.even? }
+  # [1, 2, 3, 4, 5].my_select { |i| puts i.even? }
 
-def my_all?
-  self.my_each { |i|
-    unless yield(i)
-      return false
+  def my_all?
+    my_each do |i|
+      return false unless yield(i)
     end
-    }
-    return true
-end
-
-#[1, 2, 3, 4, 5].all? { |i| puts i < 5 }
-
-def my_any?
-  self.my_each { |i|
-    if yield(i)
-      return true
-    end
-  }
-  return false
-end
-
-#[1, 2, 3, 4, 5].any? { |i| puts i < 6 }
-
-def my_none?
-  self.my_each { |i|
-    if yield(i)
-      return false
-    end
-  }
-  return true
-end
-
-#[1, 2, 3, 4, 5].my_none? { |i| puts i < 10 }
-
-def my_count
-  count = 0
-  self.my_each { |i|
-    if yield(i)
-      count += 1
-    end
-  }
-  return count
-end
-
-#puts [1, 2, 3, 4, 5].my_count { |i| i % 2 == 0 }
-
-=begin
-
-(original map method)
-
-def my_map
-  result =[]
-
-  if self.my_each { |i| result << yield(i)}
-    return result
-  else
-    return self
+    true
   end
 
-end
-[1, 2, 3, 4, 5].my_map { |i| puts i**2 }
+  # [1, 2, 3, 4, 5].all? { |i| puts i < 5 }
 
-def my_map(&proc)(proc map method)
-  result = []
-  self.my_each { |i| results << proc.call(i)}
-    return result
-end
-
-proc = Proc.new { |i| i**2 }
-puts [1, 2, 3].my_map(proc)
-
-=end
-
-def my_map(proc=nil)
-  result = []
-  if self.my_each { |i| results << proc.call(yield(i))}
-  return result
-  else
-  return self
-end
-end
-
-def my_inject
-  result = self[0]
-  self[1..-1].my_each do |element|
-    result = yield(result, element)
+  def my_any?
+    my_each do |i|
+      return true if yield(i)
+    end
+    false
   end
-  result
-end
+
+  # [1, 2, 3, 4, 5].any? { |i| puts i < 6 }
+
+  def my_none?
+    my_each do |i|
+      return false if yield(i)
+    end
+    true
+  end
+
+  # [1, 2, 3, 4, 5].my_none? { |i| puts i < 10 }
+
+  def my_count
+    count = 0
+    my_each do |i|
+      count += 1 if yield(i)
+    end
+    count
+  end
+
+  # puts [1, 2, 3, 4, 5].my_count { |i| i % 2 == 0 }
+
+  #
+  # (original map method)
+  #
+  # def my_map
+  #   result =[]
+  #
+  #   if self.my_each { |i| result << yield(i)}
+  #     return result
+  #   else
+  #     return self
+  #   end
+  #
+  # end
+  # [1, 2, 3, 4, 5].my_map { |i| puts i**2 }
+  #
+  # def my_map(&proc)(proc map method)
+  #   result = []
+  #   self.my_each { |i| results << proc.call(i)}
+  #     return result
+  # end
+  #
+  # proc = Proc.new { |i| i**2 }
+  # puts [1, 2, 3].my_map(proc)
+  #
+
+  def my_map(proc = nil)
+    result = []
+    my_each { |i| results << proc.call(yield(i)) }
+    result
+  end
+
+  def my_inject
+    result = self[0]
+    self[1..-1].my_each do |element|
+      result = yield(result, element)
+    end
+    result
+  end
 end
 
 def multiply_els(elements)
-  elements.my_inject {|product, n| product * n }
+  elements.my_inject { |product, n| product * n }
 end
 
 # puts multiply_els([1, 2, 3])
