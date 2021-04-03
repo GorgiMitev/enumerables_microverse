1b7fdff4a21fa5c8858f36bff5706283248e4b08
Gorgi <gorgimitev@gmail.com>
2021-04-03 13:24:43 +0200

    making the required changes after the code review

--- enumerables.rb
+++ enumerables.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 module Enumerable
   def my_each
     i = 0
@@ -8,8 +9,6 @@ module Enumerable
     end
   end
 
-  #[1, 2, 3, 4, 5].my_each { |i| puts i**2 }
-
   def my_each_with_index
     i = 0
     return to_enum unless block_given?
@@ -19,99 +18,98 @@ module Enumerable
     end
   end
 
-  #[1, 2, 3, 4, 5].my_each_with_index { |i, index| puts i.to_s + " index:" + index.to_s }
-
   def my_select
     return to_enum unless block_given?
 
     arr = []
-    my_each do |i| 
-      arr << i if yield i 
+    my_each do |i|
+      arr << i if yield i
     end
     arr
   end
 
-  #puts [1, 2, 3, 4, 5].my_select { |i| i.even? }
-
-  def my_all?
-    my_each do |i|
-      return false unless yield(i)
+  def my_all?(para = nil)
+    if block_given?
+      my_each { |i| return false unless yield i }
+    elsif para
+      my_each { |i| return false unless match?(i, para) }
+    else
+      my_each { |i| return false unless i }
     end
     true
   end
 
-  puts [1, 2, 3, 4, 5].all? { |i| i <= 5 }
-  
-  def my_any?
-    my_each do |i|
-      return true if yield(i)
+  def my_any?(para = nil)
+    if block_given?
+      my_each { |i| return true if yield i }
+    elsif para
+      my_each { |i| return true if match?(i, para) }
+    else
+      my_each { |i| return true if i }
     end
     false
   end
 
-  puts [1, 2, 3, 4, 5].any? { |i| i < 2 }
-
-  def my_none?
-    my_each do |i|
-      return false if yield(i)
+  def my_none?(para = nil)
+    if block_given?
+      my_each { |i| return false if yield i }
+    elsif para
+      my_each { |i| return false if match?(i, para) }
+    else
+      my_each { |i| return false if i }
     end
     true
   end
 
-  # [1, 2, 3, 4, 5].my_none? { |i| puts i < 10 }
-
   def my_count
     count = 0
-    my_each do |i|
-      count += 1 if yield(i)
+    if block_given?
+      my_each do |i|
+        count += 1 if yield(i)
+      end
     end
     count
   end
 
-  # puts [1, 2, 3, 4, 5].my_count { |i| i % 2 == 0 }
-
-  #
-  # (original map method)
-  #
-  # def my_map
-  #   result =[]
-  #
-  #   if self.my_each { |i| result << yield(i)}
-  #     return result
-  #   else
-  #     return self
-  #   end
-  #
-  # end
-  # [1, 2, 3, 4, 5].my_map { |i| puts i**2 }
-  #
-  # def my_map(&proc)(proc map method)
-  #   result = []
-  #   self.my_each { |i| results << proc.call(i)}
-  #     return result
-  # end
-  #
-  # proc = Proc.new { |i| i**2 }
-  # puts [1, 2, 3].my_map(proc)
-  #
-
   def my_map(proc = nil)
     result = []
-    my_each { |i| results << proc.call(yield(i)) }
-    result
-  end
-
-  def my_inject
-    result = self[0]
-    self[1..-1].my_each do |element|
-      result = yield(result, element)
+    if proc
+      my_each { |i| results << proc.call(yield(i)) }
+    elsif block_given?
+      my_each { |i| new_arr << yield(i) }
+    else
+      return to_enum unless block_given?
     end
     result
   end
+
+  def my_inject(total = nil, symb = nil)
+    if (total.is_a?(Symbol) || total.is_a?(String)) && symb.nil?
+      symb = total
+      total = nil
+    end
+    if !block_given? && !symb.nil?
+      my_each { |i| total = total.nil? ? i : total.send(symb, i) }
+    else
+      my_each { |i| total = total.nil? ? i : yield(total, i) }
+    end
+    total
+  end
+
+  def match?(i, para)
+    case para
+    when Regexp
+      i =~ para
+    when Class
+      i.is_a?(para)
+    else
+      i == para
+    end
+  end
 end
+
+# rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 
 def multiply_els(elements)
-  elements.my_inject { |product, n| product * n }
+  elements.my_inject { |product, i| product * i }
 end
-
-# puts multiply_els([1, 2, 3])
