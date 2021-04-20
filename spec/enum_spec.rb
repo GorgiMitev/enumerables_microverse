b1dafecf569066ea28a62aaf5af4f9561457043f
onu henry <onuhenry24@gmail.com>
2021-04-20 10:10:49 +0100

    add test for my_each method

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -9,6 +9,12 @@ describe Enumerable do
             it 'Returns the range of numbers passed when range is given' do
               expect((1..5).my_each { |i| i }).to eql(1..5)
             end
-          end
+              it 'Returns the new array with the block given' do
+                expect(([1, 2, 3]).my_each { |i| i*2}) == ([2, 4, 6])
+              end
+              it 'Returns a key and a value' do
+                expect(({goat: 'bleat', snake: 'hiss'}).my_each{ |i| i}).to eql(goat: 'bleat', snake: 'hiss')
+              end
+          end  
     end
   end
