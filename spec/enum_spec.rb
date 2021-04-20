d19cf4f90dbcb2bd564b2e2820ec36598489f7cd
Gorgi <gorgimitev@gmail.com>
2021-04-20 11:28:01 +0200

    adding the test for the my_each_with_index_method

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -1,20 +1,31 @@
 require_relative '../enumerables'
 
 describe Enumerable do
-    describe '#my_each' do
-        it 'Returns an enumerable when no block is given' do
-            expect([1, 3, 5].my_each).to be_a(Enumerable)
-          end
-          context 'when block is given' do
-            it 'Returns the range of numbers passed when range is given' do
-              expect((1..5).my_each { |i| i }).to eql(1..5)
-            end
-              it 'Returns the new array with the block given' do
-                expect(([1, 2, 3]).my_each { |i| i*2}) == ([2, 4, 6])
-              end
-              it 'Returns a key and a value' do
-                expect(({goat: 'bleat', snake: 'hiss'}).my_each{ |i| i}).to eql(goat: 'bleat', snake: 'hiss')
-              end
-          end  
+  describe '#my_each' do
+    it 'Returns an enumerable when no block is given' do
+      expect([1, 3, 5].my_each).to be_a(Enumerable)
+    end
+    context 'when block is given' do
+      it 'Returns the range of numbers passed when range is given' do
+        expect((1..5).my_each { |i| i }).to eql(1..5)
+      end
+      it 'Returns the new array with the block given' do
+        expect([1, 2, 3].my_each { |i| i * 2 }) == [2, 4, 6]
+      end
+      it 'Returns a key and a value' do
+        expect({ goat: 'bleat', snake: 'hiss' }.my_each { |i| i }).to eql(goat: 'bleat', snake: 'hiss')
+      end
     end
   end
+
+  describe '#my_each_with_index' do
+    it 'Returns an enumerable when no block is given' do
+      expect([1, 3, 5].my_each_with_index).to be_a(Enumerable)
+    end
+    context 'when block is given' do
+      it 'Returnes the values with their idices' do
+        expect([1, 3, 5].my_each_with_index { |el, idx| puts "#{el} #{idx}" }) === [1, 3, 5]
+      end
+    end
+  end
+end
